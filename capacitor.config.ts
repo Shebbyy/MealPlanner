96059691d719c05a1f6fdf992a3b9b78c25e0a2e
Sebastian Gradwohl <sebiplay15@gmx.at>
2022-10-24 21:49:25 +0200

    Initial Commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Shebby Meal Prepper',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
