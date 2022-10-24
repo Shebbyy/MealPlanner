5999348a33aebcb74c57cfc2dd8b7a13df726457
Sebastian Gradwohl <sebiplay15@gmx.at>
2022-10-24 23:24:33 +0200

    - Added a Tab navigation - Added Home Page - Added a Calendar Page for the MealPlanner (to be implemented later)

---
+++ src/app/pages/tabnav/tabnav-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { TabnavPage } from './tabnav.page';
+
+const routes: Routes = [
+  {
+    path: 'tab-nav',
+    component: TabnavPage,
+    children: [
+      {
+        path: 'home',
+        loadChildren: () => import('../home/home.module').then(m => m.HomePageModule)
+      },
+      {
+        path: 'calendar',
+        loadChildren: () => import('../calendar/calendar.module').then(m => m.CalendarPageModule)
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/tab-nav/home',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class TabnavPageRoutingModule {}
